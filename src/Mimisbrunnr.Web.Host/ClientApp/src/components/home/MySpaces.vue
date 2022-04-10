3cf08f7a7ff7c430b9cc6db229b71a334975f9bc
Клабуков Эрик Константинович <klabukov@skbkontur.ru>
2022-04-10 12:15:15 +0500

    Add my spaces sceleton ( favorites spaces )

---
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/components/home/MySpaces.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-list-group>
+        <b-list-group-item button><b-skeleton animation="fade" width="70%"></b-skeleton></b-list-group-item>
+        <b-list-group-item button><b-skeleton animation="fade" width="40%"></b-skeleton></b-list-group-item>
+        <b-list-group-item button disabled><b-skeleton animation="fade" width="80%"></b-skeleton></b-list-group-item>
+        <b-list-group-item button><b-skeleton animation="fade" width="20%"></b-skeleton></b-list-group-item>
+        <b-list-group-item button><b-skeleton animation="fade" width="25%"></b-skeleton></b-list-group-item>
+    </b-list-group>
+</template>
+
+<script>
+export default {
+    name: "MySpaces"
+}
+</script>
+
+<style>
+
+</style>
--- src/Mimisbrunnr.Web.Host/ClientApp/src/views/Home.vue
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/views/Home.vue
@@ -14,8 +14,19 @@
             <h5 style="text-align: left">DISCOVER</h5>
             <br />
           </template>
+          <template #tabs-end v-if="this.$store.state.application.profile">
+            <br />
+            <h5 style="text-align: left">
+              MY SPACES
+              <b-link class="favorites-all-link" to="/favorites">ALL</b-link>
+            </h5>
+            <br />
+            <MySpaces/>
+          </template>
+
           <Updates />
           <RecentlyVisited />
+
         </b-tabs>
       </b-card>
     </b-container>
@@ -25,11 +36,13 @@
 <script>
 import Updates from "@/components/home/Updates.vue";
 import RecentlyVisited from "@/components/home/RecentlyVisited.vue";
+import MySpaces from "@/components/home/MySpaces.vue";
 export default {
   name: "home",
   components: {
     Updates,
     RecentlyVisited,
+    MySpaces
   },
 };
 </script>
@@ -44,5 +57,9 @@ export default {
 .home-nav-active-item {
   background-color: rgba(9, 30, 66, 0.08) !important;
   color: #42526e !important;
+}
+.favorites-all-link {
+  text-decoration: none;
+  float: right;
 }
 </style>
