6a4d010747b88c08127de90aa04f5ac23851a0b1
Erik Klabukov <amect@ya.ru>
2022-10-02 11:27:00 +0500

    Add user token ui

---
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/components/people/profile/SettingsModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-modal 
+    id="profile-settings-modal" 
+    :title="$t('profile.settings.title')"
+    hide-footer
+    centered 
+    size="lg"
+  >
+    <b-tabs pills card vertical>
+      <api-tokens/>
+      <b-tab :title="$t('profile.settings.profile.title')" disabled></b-tab>
+      <b-tab :title="$t('profile.settings.other.title')" disabled></b-tab>
+    </b-tabs>
+  </b-modal>
+</template>
+
+<script>
+import ApiTokens from '@/components/people/profile/settings/ApiTokens.vue';
+export default {
+  components: { 
+    ApiTokens 
+  },
+  name: "SettingsModal",
+};
+</script>
+
+<style>
+#profile-settings-modal .modal-body {
+    padding: unset;
+}
+</style>
---
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/components/people/profile/settings/ApiTokens.vue
@@ -0,0 +1,94 @@
+<template>
+  <b-tab :title="$t('profile.settings.tokens.title')" active no-body>
+    <b-card
+      :title="$t('profile.settings.tokens.title')"
+      :sub-title="$t('profile.settings.tokens.description')"
+    >
+      <b-list-group>
+        <b-list-group-item
+          button
+          v-for="token in this.tokens"
+          :key="token.id"
+          :class="[token.revoked || new Date(token.expired) < new Date() ? 'revoked-token' : '']"
+        >
+          {{ $t("profile.settings.tokens.notBefore") }}
+          {{ new Date(token.created).toLocaleString() }}
+          {{ $t("profile.settings.tokens.notAfter") }}
+          {{ new Date(token.expired).toLocaleString() }}
+          <b-button
+            v-if="!token.revoked && new Date(token.expired) > new Date()"
+            variant="outline-danger"
+            style="float: right"
+            v-on:click="revoke(token)"
+          >
+            {{ $t("profile.settings.tokens.revoke") }}
+          </b-button>
+        </b-list-group-item>
+      </b-list-group>
+      <hr />
+      <b-card-sub-title>{{
+        $t("profile.settings.tokens.createNewDescription")
+      }}</b-card-sub-title>
+      <b-input-group v-if="!token" :prepend="$t('profile.settings.tokens.days')" class="mt-3">
+        <b-form-input v-model="tokenLifetimeInDays" required></b-form-input>
+        <b-input-group-append>
+          <b-button size="sm" text="Button" variant="success" @click="create">{{
+            $t("profile.settings.tokens.create")
+          }}</b-button>
+        </b-input-group-append>
+      </b-input-group>
+      <b-form-textarea v-if="token" plaintext :value="token"></b-form-textarea>
+    </b-card>
+  </b-tab>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ApiTokens",
+  data() {
+    return {
+      tokens: [],
+      tokenLifetimeInDays: "180",
+      token: null,
+    };
+  },
+  methods: {
+    load: async function () {
+      var tokenRequest = await axios.get("/api/account/token");
+      this.tokens = tokenRequest.data;
+    },
+    revoke: async function (token) {
+      await axios.delete(`/api/account/token/${token.id}`);
+      await this.load();
+    },
+    create: async function () {
+      var tokenLifetime = parseInt(this.tokenLifetimeInDays);
+      if (isNaN(tokenLifetime) || tokenLifetime < 1) {
+        this.$bvToast.toast("Invalid token lifetime", {
+          title: `Token creation error`,
+          variant: "danger",
+          solid: true,
+        });
+        return;
+      }
+
+      var tokenLifetimeTimeSpan = `${tokenLifetime}.00:00:00`
+      var tokenRequest = await axios.post(`/api/account/token`, {
+        "lifetime": tokenLifetimeTimeSpan
+      });
+      this.token = tokenRequest.data.token;
+      await this.load();
+    },
+  },
+  mounted: function () {
+    this.load();
+  },
+};
+</script>
+
+<style>
+.revoked-token {
+  text-decoration: line-through !important;
+}
+</style>
