126b19156bd54516203f6d9bf28cfcd6c19fe7fe
Klabukov Erik <AMECT@ya.ru>
2023-01-02 22:53:55 +0300

    using System.Linq;

--- src/Mimisbrunnr.Infrastructure.MapperlyExtensionsGenerator/MapperlyExtensionsGenerator.cs
+++ src/Mimisbrunnr.Infrastructure.MapperlyExtensionsGenerator/MapperlyExtensionsGenerator.cs
@@ -5,6 +5,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Mimisbrunnr.Infrastructure.MapperlyExtensionsGenerator
 {
