7f8f7c25b479fd5ef8f973490c09b1f06e3b95b5
Klabukov Erik <AMECT@ya.ru>
2023-06-16 21:01:55 +0300

    Add custom action for relative links in pages for use spa route nether then default page go (#38)
    ExpandTree after change page

--- src/Mimisbrunnr.Web.Host/ClientApp/src/components/space/Page.vue
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/components/space/Page.vue
@@ -91,6 +91,7 @@
         :toc="true"
         :html="this.$store.state.application.info.allowHtml"
         :source="this.page.content"
+        id="page-content"
       ></vue-markdown>
     </div>
     <br />
@@ -118,6 +119,7 @@ import {
   BIconStar,
   BIconThreeDots,
 } from "bootstrap-vue";
+import { replaceRelativeLinksToRoute } from "@/services/Utils";
 const VueMarkdown = () => import(/* webpackChunkName: "vue-markdown-component" */"@/thirdparty/VueMarkdown");
 import FavoriteService from "@/services/favoriteService";
 import PageService from "@/services/pageService";
@@ -240,6 +242,7 @@ export default {
       this.loadComments();
       setTimeout(() => hljs.highlightAll(), 100);
       setTimeout(this.scrollToAnchor, 100);
+      setTimeout(replaceRelativeLinksToRoute, 250, "page-content");
     },
   },
   mounted: function () {
@@ -248,6 +251,7 @@ export default {
     this.loadComments();
     setTimeout(() => hljs.highlightAll(), 100);
     setTimeout(this.scrollToAnchor, 100);
+    setTimeout(replaceRelativeLinksToRoute, 250, "page-content");
   },
 };
 </script>
