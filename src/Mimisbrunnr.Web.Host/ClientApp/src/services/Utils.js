246e5aa6aa078febcb98fece81797b7c694f5c4b
Klabukov Erik <amect@ya.ru>
2023-06-16 22:57:11 +0500

    Add custom action for relative links in pages for use spa route nether then default page go ExpandTree after change page

--- src/Mimisbrunnr.Web.Host/ClientApp/src/services/Utils.js
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/services/Utils.js
@@ -1,4 +1,5 @@
 import { BToast } from 'bootstrap-vue'
+import router from '@/router'
 var bootStrapToaster = null;
 
 export function debounce(fn, delay) {
@@ -43,4 +44,22 @@ export function isImageFile(name) {
         name.toLowerCase().endsWith(".gif") ||
         name.toLowerCase().endsWith(".svg")
     );
+}
+
+export function replaceRelativeLinksToRoute(elementId){
+    var block = document.getElementById(elementId);
+    if(!block) return;
+
+    var links = block.getElementsByTagName("a");
+    if(!links) return;
+
+    var linksForReplace = Array.from(links).filter(x =>  x.getAttribute("href").startsWith("/") && !x.getAttribute("href").startsWith("/api/"));
+    if(!linksForReplace) return;
+
+    for(let link of linksForReplace){
+        link.addEventListener("click", function (e) {
+            e.preventDefault(); 
+            router.push(e.target.getAttribute("href"));
+        })
+    }
 }
