8c0e2f04fab7dd40e3c33c260a249856bfc2cf91
Klabukov Erik <amect@ya.ru>
2024-06-08 22:47:56 +0500

    Macro plugin is WORK (changed macro command to html block)

--- src/Mimisbrunnr.Web.Host/ClientApp/src/services/markdown/markdown-it-macro-plugin.js
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/services/markdown/markdown-it-macro-plugin.js
@@ -1,43 +1,30 @@
-const macroRegex = /{{macro:name=([^&]+)&id=([^&]+)&([^}]+)}}/g;
-
 function macroPlugin(md) {
-  // Создаем новый тип токена
-  const macroToken = (state, startLine, endLine, silent) => {
-    let pos = state.bMarks[startLine] + state.tShift[startLine];
-    let max = state.eMarks[startLine];
-
-    if (pos < max) {
-        let match = macroRegex.exec(state.src.slice(pos));
-
-        if(!match) 
-            return false;
-
-        const [, name, id, params] = match;
-        const ariaParams = params.replace(/&/g, "&amp;");
-        const token = new state.Token("macro", `div id="macro_${id}" aria-name="${name}" aria-params="${ariaParams}"></div`, 0);
-        token.block = true;
-        token.content = match[0];
-        token.markup = match[0];
-        token.map = [startLine, state.bMarks[startLine] + pos, startLine, state.bMarks[startLine] + pos + match[0].length];
-
-        state.src = state.src.slice(0, pos) + state.src.slice(pos + match[0].length);
-        state.tShift[startLine] += -match[0].length;
-        for (let i = startLine + 1; i <= endLine; i++) {
-            state.tShift[i] += -match[0].length;
-        }
-
+    function replaceMacro(state, start, end, silent) {
+      const tail = state.src.slice(end);
+      const match = tail.match(/^\{\{macro:name=([^\|]+)\|id=([^\|]+)\|([^}]+)\}\}/);
+  
+      if (match) {
         if (!silent) {
-            state.tokens.push(token);
+          const token = state.push('html_inline', '', 0);
+          const [, name, id, params] = match;
+  
+          const paramArray = params.split('|');
+          const paramKeyValuePairs = paramArray.map(param => param.split('='));
+          const paramAttributes = paramKeyValuePairs.map(([key, value]) => `${key}="${value}"`).join('&');
+          const encodedParams = encodeURI(paramAttributes);
+  
+          token.block = true;
+          token.content = `<div id="macro_${id}" aria-name="${name}" aria-params="${encodedParams}"></div>`;
+          token.map = [start, state.pos];
+          token.markup = '{{'; // markup will be set to '{{' for proper handling
         }
-
+        state.pos = state.pos + match.index + match[0].length;
         return true;
+      }
+      return false;
     }
-
-    return false;
-  };
-
-  // Зарегистрируем новый тип токена в markdown-it
-  md.block.tokenizer.register("macro", macroToken, "paragraph", "blockquote", "list", "html_block");
-}
-
-export default macroPlugin;
+  
+    md.inline.ruler.after('emphasis', 'macro_replace', replaceMacro);
+  }
+  
+  export default macroPlugin;
