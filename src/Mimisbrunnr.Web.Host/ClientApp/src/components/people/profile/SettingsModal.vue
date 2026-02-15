805b9adf50cb4132e697ea47a421a8b7d0fda5bc
Klabukov Erik <amect@ya.ru>
2026-02-15 21:48:56 +0500

    Move profile additional info edit from panel to settings modal

--- src/Mimisbrunnr.Web.Host/ClientApp/src/components/people/profile/SettingsModal.vue
+++ src/Mimisbrunnr.Web.Host/ClientApp/src/components/people/profile/SettingsModal.vue
@@ -6,10 +6,10 @@
     centered 
     size="lg"
   >
-    <b-tabs pills card vertical>
+<b-tabs :pills="isVertical" :card="isVertical" :vertical="isVertical">
       <api-tokens/>
       <user-groups/>
-      <b-tab :title="$t('profile.settings.profile.title')" disabled></b-tab>
+      <additional-info-tab :profile="profile"/>
       <b-tab :title="$t('profile.settings.other.title')" disabled></b-tab>
     </b-tabs>
   </b-modal>
@@ -18,12 +18,38 @@
 <script>
 import ApiTokens from '@/components/people/profile/settings/ApiTokens.vue';
 import UserGroups from '@/components/people/profile/settings/UserGroups.vue';
+import AdditionalInfoTab from '@/components/people/profile/settings/AdditionalInfoTab.vue';
 export default {
   components: { 
     ApiTokens,
-    UserGroups
+    UserGroups,
+    AdditionalInfoTab
   },
   name: "SettingsModal",
+  props: {
+    profile: Object,
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth
+    };
+  },
+  computed: {
+    isVertical() {
+      return this.windowWidth > 440;
+    }
+  },
+  created() {
+    window.addEventListener('resize', this.updateWindowWidth);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.updateWindowWidth);
+  },
+  methods: {
+    updateWindowWidth() {
+      this.windowWidth = window.innerWidth;
+    }
+  }
 };
 </script>
 
